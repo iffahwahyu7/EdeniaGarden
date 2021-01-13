683ab874c76181ee867c504befa01f31a4a185d7
iffahwahyu7 <61259779+iffahwahyu7@users.noreply.github.com>
2021-01-13 17:50:05 +0700

    Add files via upload

---
+++ database/migrations/2020_12_17_093821_create_inventories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->string('id',4);
+            $table->string('nama',30);
+            $table->string('jenis',30);
+            $table->integer('stok');
+            $table->timestamps('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
