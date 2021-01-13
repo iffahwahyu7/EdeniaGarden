d9594d0729eca40fdda3ae20f4e34fd98c6439eb
iffahwahyu7 <61259779+iffahwahyu7@users.noreply.github.com>
2021-01-13 17:48:15 +0700

    Add files via upload

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function beranda()
+    {
+        return view('beranda');
+    }
+
+    public function bunga()
+    {
+        return view('bunga');
+    }
+}
